bf98e2920fe1eaaaa26afd6e3637eae893cf16d8
sameerjohn1 <sameerprogrammer5@gmail.com>
2024-04-10 10:22:00 +0500

    mongodb connection created successfully

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/connectDb.js";
 
+dotenv.config();
+
+connectDB();
 const app = express();
 
-app.listen(5000, () => console.log("Server started at 5000 port"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server started at http://localhost:${PORT}`)
+);
