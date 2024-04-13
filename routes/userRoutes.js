a58f60d4fa3d1aa2c631c27d626389230b5101e7
sameerjohn1 <sameerprogrammer5@gmail.com>
2024-04-13 10:01:08 +0500

    logout route added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { signupUser, loginUser } from "../controllers/userControllers.js";
+import {
+  signupUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/userControllers.js";
 
 const router = express.Router();
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
+router.post("/logout", logoutUser);
 
 export default router;
