fa6831b631659716280049c655684a9a8fc7382f
sameerjohn1 <sameerprogrammer5@gmail.com>
2024-04-15 10:39:54 +0500

    initial added comment

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,7 +15,7 @@ router.get("/profile/:username", getUserProfile);
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
-router.post("/follow/:id", protectRoute, followUnFollowUser);
+router.post("/follow/:id", protectRoute, followUnFollowUser); //Toggle state(follow/unfollow)
 router.post("/update/:id", protectRoute, updateUser);
 
 export default router;
