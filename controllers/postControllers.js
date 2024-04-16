28353d98194a881e9b209731eb919c213ad836d3
sameerjohn1 <sameerprogrammer5@gmail.com>
2024-04-16 09:44:52 +0500

    add delete post controller

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -51,4 +51,24 @@ const getPost = async (req, res) => {
   }
 };
 
-export { createPost, getPost };
+// delete post
+const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    if (post.postedBy.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "Unauthorized to delete post" });
+    }
+
+    await Post.findByIdAndDelete(req.params.id);
+
+    return res.status(200).json({ message: "Post deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { createPost, getPost, deletePost };
