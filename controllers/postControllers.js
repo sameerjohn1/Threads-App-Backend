0ee4766490f36ef1f32520bb7028d9b0717414d4
sameerjohn1 <sameerprogrammer5@gmail.com>
2024-04-16 16:28:35 +0500

    get feed posts controller added

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -130,4 +130,32 @@ const replyToPost = async (req, res) => {
   }
 };
 
-export { createPost, getPost, deletePost, likeUnlikePost, replyToPost };
+// get feed posts
+const getFeedPosts = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const following = user.following;
+
+    const feedPosts = await Post.find({ postedBy: { $in: following } }).sort({
+      createdAt: -1,
+    });
+
+    res.status(200).json(feedPosts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export {
+  createPost,
+  getPost,
+  deletePost,
+  likeUnlikePost,
+  replyToPost,
+  getFeedPosts,
+};
