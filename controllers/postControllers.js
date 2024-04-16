f3698b46369aa6d5b8ddeeca9ecf3ae73f9a7d8c
sameerjohn1 <sameerprogrammer5@gmail.com>
2024-04-16 10:31:01 +0500

    added like/unlike post controller

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -71,4 +71,33 @@ const deletePost = async (req, res) => {
   }
 };
 
-export { createPost, getPost, deletePost };
+//  like / unlike post
+const likeUnlikePost = async (req, res) => {
+  try {
+    const { id: postId } = req.params;
+    const userId = req.user._id;
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    const userLikedPost = post.likes.includes(userId);
+
+    if (userLikedPost) {
+      // unlike post
+      await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+      res.status(200).json({ message: "Post unliked successfully" });
+    } else {
+      // like post
+      post.likes.push(userId);
+      await post.save();
+      res.status(200).json({ message: "Post liked successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { createPost, getPost, deletePost, likeUnlikePost };
