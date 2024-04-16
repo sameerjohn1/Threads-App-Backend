6f669d520f5888d727833cd139a9af03137cf2fd
sameerjohn1 <sameerprogrammer5@gmail.com>
2024-04-16 10:47:01 +0500

    addes reply to post controller

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import Post from "../models/postSchema.js";
 
+// create post
 const createPost = async (req, res) => {
   try {
     const { postedBy, text, img } = req.body;
@@ -100,4 +101,33 @@ const likeUnlikePost = async (req, res) => {
   }
 };
 
-export { createPost, getPost, deletePost, likeUnlikePost };
+// reply to post
+const replyToPost = async (req, res) => {
+  try {
+    const { text } = req.body;
+    const postId = req.params.id;
+    const userId = req.user._id;
+    const userProfilePic = req.user.profilePic;
+    const username = req.user.username;
+
+    if (!text) {
+      return res.status(400).json({ message: "Text field is required" });
+    }
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(400).json({ message: "Post not found" });
+    }
+
+    const reply = { userId, text, userProfilePic, username };
+
+    post.replies.push(reply);
+    await post.save();
+
+    res.status(200).json({ message: "Reply added successfully", post });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { createPost, getPost, deletePost, likeUnlikePost, replyToPost };
