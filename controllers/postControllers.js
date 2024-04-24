ddf850157c16f2d35b8837b3f691136ca0349d9f
sameerjohn1 <sameerprogrammer5@gmail.com>
2024-04-24 16:47:14 +0500

    add get user posts controller

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -158,6 +158,24 @@ const getFeedPosts = async (req, res) => {
   }
 };
 
+const getUserPosts = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const posts = await Post.find({ postedBy: user._id }).sort({
+      createdAt: -1,
+    });
+
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
 export {
   createPost,
   getPost,
@@ -165,4 +183,5 @@ export {
   likeUnlikePost,
   replyToPost,
   getFeedPosts,
+  getUserPosts,
 };
