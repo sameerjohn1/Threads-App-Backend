a88c9c0829e79f1498e08485a6beb7f35d01da2a
sameerjohn1 <sameerprogrammer5@gmail.com>
2024-04-25 18:27:25 +0500

    initial changes for removing image from cloudinary

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -53,7 +53,7 @@ const getPost = async (req, res) => {
       return res.status(404).json({ error: "Post not found" });
     }
 
-    res.status(200).json({ post });
+    res.status(200).json(post);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -69,6 +69,11 @@ const deletePost = async (req, res) => {
 
     if (post.postedBy.toString() !== req.user._id.toString()) {
       return res.status(401).json({ error: "Unauthorized to delete post" });
+    }
+
+    if (post.img) {
+      const imgId = post.img.split("/").pop().split(".")[0];
+      await cloudinary.uploader.destroy(imgId);
     }
 
     await Post.findByIdAndDelete(req.params.id);
